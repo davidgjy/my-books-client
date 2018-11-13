b0b0ebeb96901a8990ccb30cbb2e8d31b27019fd
Kelvin Gu <jgu55@its.jnj.com>
2018-11-13 14:03:05 +0800

    add fake data and authentication by json-server

---
+++ source/angular/book-library/data.js
@@ -0,0 +1,17 @@
+module.exports = function () {
+  return {
+    books: [
+      { id: 1, name: "Java 8实战", category: "JAVA", owner: "ADC", type: "PAPER BOOK", location: "8F", team: "EDG" },
+      { id: 2, name: "Spring微服务实战", category: "MicroService", owner: "ADC", type: "PAPER BOOK", location: "8F", team: "EDG" },
+      { id: 3, name: "Akka实战：快速构建高可用分布式应用", category: "BigData", owner: "ADC", type: "PAPER BOOK", location: "8F", team: "EDG" },
+      { id: 4, name: "Docker实战", category: "DevOps", owner: "ADC", type: "PAPER BOOK", location: "8F", team: "EDG" },
+      { id: 5, name: "Java虚拟机规范", category: "JAVA", owner: "ADC", type: "PAPER BOOK", location: "8F", team: "EDG" },
+      { id: 6, name: "MySQL技术内幕：InnoDB存储引擎（第2版）", category: "Database", owner: "ADC", type: "PAPER BOOK", location: "8F", team: "Digital" },
+      { id: 7, name: "PHP7内核剖析", category: "PHP", owner: "ADC", type: "PAPER BOOK", location: "8F", team: "Digital" },
+      { id: 8, name: "响应式架构：消息模式Actor实现与Scala、Akka应用集成", category: "BigData", owner: "ADC", type: "PAPER BOOK", location: "8F", team: "EDG" },
+      { id: 9, name: "码出高效：Java开发手册", category: "JAVA", owner: "KG", type: "PAPER BOOK", location: "", team: "" },
+      { id: 10, name: "Spring Cloud与Docker微服务架构实战", category: "MicroService", owner: "KG", type: "PAPER BOOK", location: "", team: "" }
+    ],
+    orders: []
+  }
+};
